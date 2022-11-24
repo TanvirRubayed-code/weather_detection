527c91c5f3016723c27f1474520b4db5d77f1a36
Hamza Mohtadi <hamzamohtadi225@gmail.com>
2022-11-24 22:29:06 +0600

    commit from  hamjas branch

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,9 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-          hi 
+          
         </h1>
-      //from hamja-branch
+      <h1>hi</h1>  
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
