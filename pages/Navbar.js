ebc1376033ae179fbe63e72378af26b45b2f6690
TanvirRubayed-code <tanvirrubayedtauhid@gmail.com>
2022-11-26 01:04:06 +0600

    basic structured added

---
+++ pages/Navbar.js
@@ -0,0 +1,7 @@
+
+function Navbar() {
+    return <div>
+        Navbar is here
+    </div>
+}
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import HomePage from "./Home";
 export default function Home() {
-  return (
-
-   <div>
-     Tauhid 
-    
-  
-
-      Successfully pull from hamja branch.
-    </div>
-  )
+  return <div>
+    <HomePage></HomePage>
+  </div>;
 }
