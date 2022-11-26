b65e7d65458bf69b1252fad004dd776c23cc55d8
saifuddinsabbir <saifuddin.sabbir@gmail.com>
2022-11-26 16:45:59 +0600

    components are added

--- components/Home.js
+++ components/Home.js
@@ -1,8 +1,17 @@
 import Navbar from "./Navbar";
+import Banner from "./Banner"
+import Products from "./products"
+import BestProducts from "./BestProduct"
+import Footer from "./Footer";
+import styles from "../styles/Home.module.css"
 
-function Home(){
+
+function Home() {
     return <div>
-        <Navbar></Navbar>
+        <Banner></Banner>
+        <Products></Products>
+        <BestProducts></BestProducts>
+        <Footer></Footer>
     </div>
 }
 export default Home;
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,5 @@ export default function Home() {
 
   return <div>
     <HomePage></HomePage>
-    Hello World
-    New item added
   </div>;
 }
