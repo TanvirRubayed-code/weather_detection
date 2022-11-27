3b751e7b0e8c48091db686fd6240db53f29b359a
Hamza Mohtadi <hamzamohtadi225@gmail.com>
2022-11-27 10:59:06 +0600

    login page updated without using bootstrap

---
+++ pages/login.js
@@ -0,0 +1,29 @@
+
+
+function Login() {
+
+  return  <div class="main-container">
+	<div class="login-box">
+		<div class="header">
+			<h2>Login Page</h2>
+		</div>
+		<div class="login">
+			<div class="form-control1">
+				<input type="text" placeholder="user name" class="tbox" required />
+			</div>
+			<div class="form-control1">
+				<input type="password" placeholder="*********" class="tbox" required />
+			</div>
+			<div class="form-control1">
+				<input type="submit" value="Login Now" class="btn" />
+			</div>
+			<div class="forget-box">
+				<a href="#" class="link">forget password</a>
+			</div>
+		</div>
+	</div>
+    </div>
+  
+  
+}
+export default Login
