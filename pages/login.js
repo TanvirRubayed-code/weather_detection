2c3cf090609d0d524aea0307c78599c270d60b25
Hamza Mohtadi <hamzamohtadi225@gmail.com>
2022-11-27 22:21:43 +0600

    login and signup page done by hamja branch

--- pages/login.js
+++ pages/login.js
@@ -1,29 +1,26 @@
+ 
+import styles from '../styles/Login.module.css';
 
-
-function Login() {
-
-  return  <div class="main-container">
-	<div class="login-box">
-		<div class="header">
-			<h2>Login Page</h2>
-		</div>
-		<div class="login">
-			<div class="form-control1">
-				<input type="text" placeholder="user name" class="tbox" required />
-			</div>
-			<div class="form-control1">
-				<input type="password" placeholder="*********" class="tbox" required />
-			</div>
-			<div class="form-control1">
-				<input type="submit" value="Login Now" class="btn" />
-			</div>
-			<div class="forget-box">
-				<a href="#" class="link">forget password</a>
-			</div>
-		</div>
+function Login (){
+return <div class="login-form">
+<form>
+  <h1>Login</h1>
+  <div class="content">
+	<div class="input-field">
+	  <input type="email" placeholder="Email" autocomplete="nope"/>
 	</div>
-    </div>
-  
-  
+	<div class="input-field">
+	  <input type="password" placeholder="Password" autocomplete="new-password"/>
+	</div>
+	<a href="#" class="link">Forgot Your Password?</a>
+  </div>
+  <div class="action">
+	<button>Register</button>
+	<button>Sign in</button>
+  </div>
+</form>
+</div>
+
 }
+
 export default Login
