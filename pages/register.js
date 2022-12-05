46d4524f6e421845d423328f1ca37efc0cd5ecc2
Hamza Mohtadi <hamzamohtadi225@gmail.com>
2022-12-05 22:47:02 +0600

    last kora register page update by hamja-branch

--- pages/register.js
+++ pages/register.js
@@ -1,66 +1,46 @@
  
-
+import styles from '../styles/Register.module.css';
+ import Navbar from "../components/Navbar"
+ import Footer from "../components/Footer"
 
 function Register(){
-return <div class="login-form">
-<form>
-  <h1>Register</h1>
-  <div class="content">
-	<div class="input-field">
-	  <input type="email" placeholder="Email" autocomplete="nope"/>
-	</div>
+  return <>
+  <Navbar></Navbar>
+  <div className={styles.register_form}>
+      <form>
+          <h1>Register</h1>
+          <div className={styles.content}>
+              <div className={styles.input_field}>
+                  <input type="email" placeholder="username or e-mail" autocomplete="nope" />
+              </div>
+              <div className={styles.input_field}>
+                  <input type="password" placeholder="password" autocomplete="new-password" />
+              </div>
+              
+              <div className={styles.input_field}>
+                  <input type="password" placeholder="confirm password" autocomplete="new-password" />
+              </div>
 
-    <div class="input-field">
-	  <input type="username or e-mail" placeholder="username or e-mail" autocomplete="nope"/>
-	</div>
-    
-     <div class="input-field">
-            <input type="text" placeholder="Enter your number" required/>
+              <div class={styles.container_signin}>
+                 <h5>Already have an account? <a href="#">Sign in</a>.</h5>
+              </div>
+              
+
+              <div class ={styles.legacy}>
+              <p>  <input type="checkbox"></input> By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>  
+              </div>
+
+
           </div>
-
-	<div class="input-field">
-	  <input type="password" placeholder="Password" autocomplete="new-password"/>
-	</div>
-
-    <div class="input-field">
-	  <input type="Confirm Password" placeholder="Confirm Password" autocomplete="new-password"/>
-	</div>
-
-
-    {/* <div class="gender-details">
-          <input type="radio" name="gender" id="dot-1"/>
-          <input type="radio" name="gender" id="dot-2"/>
-          <input type="radio" name="gender" id="dot-3"/>
-          <span class="gender-title">Gender</span>
-          <div class="category">
-            <label for="dot-1">
-            <span class="dot one"></span>
-            <span class="gender">Male</span>
-          </label>
-          <label for="dot-2">
-            <span class="dot two"></span>
-            <span class="gender">Female</span>
-          </label>
-          <label for="dot-3">
-            <span class="dot three"></span>
-            <span class="gender">Prefer not to say</span>
-            </label>
+          <div className={styles.action}>
+              <button>Register</button>
           </div>
-          </div> */}
-        
-        <div class="container signin">
-    <p>Already have an account? <a href="#">Sign in</a>.</p>
+      </form>
   </div>
-  <h6>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</h6>   
+  <Footer></Footer>
+</>
 
-  </div>
-  <div class="action">
   
-	<button>Register</button>
-	
-  </div>
-</form>
-</div>
 
 }
 
