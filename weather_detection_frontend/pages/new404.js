ccac381c767c67f4797141e41807599f50940ff2
Hamza Mohtadi <hamzamohtadi225@gmail.com>
2023-06-06 09:49:38 +0600

    404 page gsap added by hamja

---
+++ weather_detection_frontend/pages/new404.js
@@ -0,0 +1,133 @@
+import { useLayoutEffect, useRef } from "react";
+import { gsap } from "gsap/dist/gsap";
+import Link from "next/link";
+
+export default function IndexPage() {
+  const root = useRef();
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.to(".page_404", { rotation: "+=360" });
+    }, root);
+
+    return () => ctx.revert();
+  }, []);
+
+  return (
+    <div className="page" ref={root}>
+         {/* <h1>Home Page</h1> 
+      <div className="box">404</div> */}
+      {/* <p>
+        <Link href="/about">About Page</Link>
+      </p> */}
+   
+
+
+<div class="page_404">
+	<div class="container">
+		<div class="row">	
+		<div class="col-sm-12 ">
+		<div class="col-sm-10 col-sm-offset-1  text-center">
+		<div class="four_zero_four_bg">
+			<h1 class="text-center ">404</h1>
+
+		</div>
+		
+		<div class="contant_box_404">
+		<h3 class="h2">
+		Look like you're lost
+		</h3>
+		
+		<p>the page you are looking for not avaible!</p>
+		
+		<a href="" class="link_404">Go to Home</a>
+	</div>
+		</div>
+		</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+       <style jsx global>{`
+        .page {
+          font-family: Avenir, Helvetica, Arial, sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+          text-align: center;
+          color: #2c3e50;
+          margin-top: 60px;
+        }
+
+        // .box {
+        //   width: 100px;
+        //   height: 100px;
+        //   border-radius: 12px;
+        //   display: flex;
+        //   align-items: center;
+        //   justify-content: center;
+        //   text-align: center;
+        //   background-color: #28a92b;
+        //   font-weight: 600;
+        //   color: #fff;
+        //   margin: 0 auto;
+        // }
+
+
+        /*======================
+        404 page
+    =======================*/
+    
+    
+    .page_404{ padding:40px 0; background:#fff; font-family: 'Arvo', serif;
+    }
+    
+    .page_404  img{ width:100%;}
+    
+    .four_zero_four_bg{
+     
+     background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
+        height: 400px;
+        background-position: center;
+     }
+     
+     
+     .four_zero_four_bg h1{
+     font-size:80px;
+     }
+     
+      .four_zero_four_bg h3{
+           font-size:80px;
+           }
+           
+           .link_404{			 
+      color: #fff!important;
+        padding: 10px 20px;
+        background: #39ac31;
+        margin: 20px 0;
+        display: inline-block;}
+      .contant_box_404{ margin-top:-50px;}
+
+      .box {
+        width: 100px;
+        height: 100px;
+        border-radius: 12px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        background-color: #28a92b;
+        font-weight: 600;
+        color: #fff;
+        margin: 0 auto;
+      }
+
+
+
+
+      `}</style> 
+    </div>
+  );
+}
